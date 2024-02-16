bb119614584760f103e2c3ecb0e5aef5905a0c96
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-16 10:04:58 +0000

    .devcontainer config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 215423025522
+var accountName = "nyal gadhiya"
+let accountCity = "surat"
+
+accountName = "nyal m gadhiya"
+accountCity = "surat ved"
+
+console.table([accountId , accountName ,accountCity])
