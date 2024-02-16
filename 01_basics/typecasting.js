09ed44b46fba7e734c0263f3b4801b87ac236d77
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-16 10:44:30 +0000

    typecasting config done

---
+++ 01_basics/typecasting.js
@@ -0,0 +1,22 @@
+let input="8420kk"
+
+console.log(typeof input);
+
+let convertInNumber=Number(input)
+
+console.log(typeof convertInNumber);
+console.log(convertInNumber)
+
+let input2=8
+
+let isbool = Boolean(input2)
+console.log(isbool)
+
+
+/* notes----------
+
+"" => false
+"nyal" => true
+0 => false
+1 =>true
+*/
