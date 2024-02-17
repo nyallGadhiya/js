97c9c48c4f9d857d366236bb8ea2dd60297595dd
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-17 13:42:47 +0000

    string01.js

---
+++ 01_basics/strings01.js
@@ -0,0 +1,21 @@
+let name="nyal_gadhiya"
+
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+console.log(name.indexOf('n'));
+
+const cname=name
+
+console.log(cname.substr(0,4));
+
+
+let comname = "     repository 0f JS                         ";
+console.log(comname.trim());
+
+let urlRepository = 'https://github.com/nyallGadhiya==js'
+
+console.log(urlRepository.replace('==','/'));
+
+console.log(urlRepository.includes('nyal55'));
+
+
