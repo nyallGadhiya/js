8c1a1729ac8a78040f73c1a96f5522c9eeb0bab3
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-19 11:28:00 +0000

    randome_dice

---
+++ 01_basics/numbers.js
@@ -0,0 +1,22 @@
+//numbers operations using Math library
+
+
+console.log(Math.abs(-84))
+
+console.log(Math.ceil(8.1))
+console.log(Math.floor(8.9))
+
+
+  console.log(Math.floor(Math.random()))
+//give any random calue between 0 and 1
+
+
+price=312.2344
+
+fprice=5316015
+
+  console.log(price.toPrecision(2));
+//preicse the number 
+
+  console.log(fprice.toLocaleString('en-IN'))
+//to print appropriate indian currency format .
---
+++ 01_basics/random_dice.js
@@ -0,0 +1,8 @@
+let min=1,max=6
+
+
+let nm=(Math.floor(Math.random()*(max-min)+min))
+
+
+
+console.log(nm)
