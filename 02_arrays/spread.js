888ec07e4ffe23b170abcc99148dc0ea50c2fe6e
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-19 13:02:55 +0000

    spread

---
+++ 02_arrays/spread.js
@@ -0,0 +1,8 @@
+let junks=['pizza','burger','lasagnia','burrito']
+let superfood=['cumins','pepper','turmeric']
+
+
+  let allFood=[...junks,...superfood]
+//spread combines multiple arrays
+
+console.log(allFood);
