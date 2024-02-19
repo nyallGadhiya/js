046e13c52dab78046f3eacb8aaf44c816e11658b
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-19 13:02:34 +0000

    01_arrays

---
+++ 02_arrays/01_arrays.js
@@ -0,0 +1,16 @@
+
+let arr=[1,2,3,4,5]
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log( arr[i]);
+// }
+
+arr.push(21)
+arr.push(84)
+console.log(arr);
+
+arr.pop()
+console.log(arr);
+
+let isPresent = arr.includes(21)
+console.log(isPresent);
