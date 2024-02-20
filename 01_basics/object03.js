3cff065611ed66ffab7a4f2451f3ec5c3e29634a
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-20 12:45:02 +0000

    object3

---
+++ 01_basics/object03.js
@@ -0,0 +1,11 @@
+let courses={
+    course:'javascript',
+    courseFee:999,
+    courseInstructor:'not decided !'
+}
+
+const {courseInstructor:mentor,course} = courses;
+
+//console.log(courseInstructor);
+console.log(mentor);
+console.log(course);
