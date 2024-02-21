9360c90624cf97aebbb737f33c7bc97e5d19d69d
nyallGadhiya <139493404+nyallGadhiya@users.noreply.github.com>
2024-02-21 09:17:21 +0000

    arrow_function

---
+++ 03_functions/arrow_function.js
@@ -0,0 +1,31 @@
+/*
+const data1={
+    name:"nyal",
+    website:undefined,
+    browser:"chrome",
+
+    welcomeMessage :function() {
+        console.log(`${this.name},you are currently browsing  ${this.browser} browser`);
+    }
+}
+
+data1.welcomeMessage();
+*/
+
+//const function1= () => {
+//    return "hello";
+//}
+
+
+const addtwo= (num1,num2) => {
+    return num1+num2;
+}
+
+console.log(addtwo(80,4))
+
+
+
+//implicit function
+  const addthree = (num1,num2,num3) => num1+num2+num3
+
+console.log(addthree(8,4,2));
